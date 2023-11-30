5ab25c041d88ecc867e8fd28f64a262dcfe4466c
GREED <pratikadhikari10@gmail.com>
2023-11-30 17:29:47 +0100

    map change

---
+++ Turns.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Turns" tilewidth="30" tileheight="29" tilecount="1" columns="1">
+ <image source="../../../Downloads/Turns.png" width="30" height="29"/>
+</tileset>
