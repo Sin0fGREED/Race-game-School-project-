12fcfbd445432b4c7c18b4bffa808cde7c1c2ec4
maktas11 <aktasmuhammed734@gmail.com>
2023-11-30 14:25:48 +0100

    added start screen plus car

--- Racing.py
+++ Racing.py
@@ -17,9 +17,14 @@ WIDTH, HEIGHT = 1390, 768  # Width and height of the game window
 FPS = 60  # Frames per second
 WHITE = (255, 255, 255)  # White color for the background
 ROAD_COLOR = (100, 100, 100)  # Gray color for the road
-CAR_COLOR = (255, 0, 0)  # Red color for the player's car
 MAP_SCALE = 3  # Scale factor for the map
-
+BLUE = (255, 255, 255)
+font = pygame.font.SysFont(None, 100)
+img = font.render('press ENTER to start', True, BLUE)
+imgRect = img.get_rect()
+imgRect.center = (WIDTH // 2, HEIGHT // 2)
+backgrounds = pygame.image.load("./mainMenu/car.jpg")
+drag = 0.04
 # Create the game window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))  # Set the size of the game window
 pygame.display.set_caption("Formula 1 Racing Course")  # Set the title of the game window
@@ -40,9 +45,8 @@ scaled_width = tmx_map.width * scaled_tilewidth  # Calculate the scaled width
 scaled_height = tmx_map.height * scaled_tileheight  # Calculate the scaled height
 
 # Create a surface for the scaled map
-original_car_image = pygame.Surface((8, 8))  # Create a surface for the original car image
-original_car_image.fill(CAR_COLOR)  # Fill the surface with the car color
-scaled_car_image = pygame.transform.scale(original_car_image, (5, 5))  # Scale the car image
+original_car_image = pygame.image.load("./Vehicles/CARS.png")  # Create a surface for the original car image
+scaled_car_image = pygame.transform.scale(original_car_image, (40, 20))  # Scale the car image
 
 # Create a rectangle for the player's car
 player_car_rect = scaled_car_image.get_rect()  # Create a rectangle for the player's car
@@ -62,82 +66,107 @@ pygame.mixer.music.play(-1)  # -1 means play indefinitely
 # Tile ID for collision (the tile ID is 253)
 COLLISION_TILE_ID = 83  # Tile ID for collision (the tile ID is 253)
 
-# def getQuadrant(x, y, centerX, centerY):
-    # rotatedX = x * math.cos(math.pi / 4) - y * math.sin(math.pi / 4)
-    # rotatedY = x * math.sin(math.pi / 4) + y * math.cos(math.pi / 4)
-    # if x < centerX and y > centerY:
-    #     return "bottomleft"
-    # if rotatedX >= centerX and rotatedY >= centerY:
-    #     return "topRight"
-    # elif rotatedX < centerX and rotatedY >= centerY:
-    #     return "right"
-    # elif rotatedX < centerX and rotatedY < centerY:
-    #     return "bottem"
-    # else:
-    #     return "bottomRight"
-
-
-
-
 def screen_to_world(screen_x, screen_y): #convert pixel location to tile location
     world_x = (screen_x / WIDTH ) * 29
     world_y = (screen_y / HEIGHT) * 16
     return [world_x, world_y]
 
 running = True  # Boolean variable to control the game loop
+checkpoint1 = False
+checkpoint2 = False
+checkpoint3 = False
+checkpoint4 = False
+game = 0
 while running: # Loop that runs while the game is running
     for event in pygame.event.get(): # Loop that gets all the events that happen in the game
         if event.type == pygame.QUIT: # If the user clicks on the close button
             running = False # Set running to False to exit the game loop
+    if game == 1:
+        # Get the pressed keys
+        keys = pygame.key.get_pressed() # Get the pressed keys
 
-    # Get the pressed keys
-    keys = pygame.key.get_pressed() # Get the pressed keys
-    player_velocity.x = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 3 # Get the horizontal velocity of the player's car
-    player_velocity.y = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * 3 # Get the vertical velocity of the player's car
+        # Adjust the horizontal velocity
+        if keys[pygame.K_RIGHT]:
+            player_velocity.x += 0.2
+        elif keys[pygame.K_LEFT]:
+            player_velocity.x -= 0.2
+
+        # Adjust the vertical velocity
+        if keys[pygame.K_DOWN]:
+            player_velocity.y += 0.2
+        elif keys[pygame.K_UP]:
+            player_velocity.y -= 0.2
+
+        # Apply drag to slow down the car when no keys are pressed
+        player_velocity *= (1 - drag)
+                
+        
+                
+        previousLocation = (player_car_rect.x, player_car_rect.y) # grabs location before collision
+        
+        player_car_rect.x += player_velocity.x
+        player_car_rect.y += player_velocity.y
+
+        tileX = math.floor(screen_to_world(player_car_rect.x, player_car_rect.y)[0])
+        tileY = math.floor(screen_to_world(player_car_rect.x, player_car_rect.y)[1])
+        xivo = screen_to_world(player_car_rect.x, player_car_rect.y)[0]
+        yivo = screen_to_world(player_car_rect.x, player_car_rect.y)[1]
+        print(f"x: {xivo:.2f} y: {yivo:.2f}, tileX: {tileX} tileY: {tileY}")
+        
+        tile_layer = tmx_map.get_layer_by_name("BORDERS")
+        tile = tile_layer.data[tileY][tileX] # gets current tile location
+        if tileX == 10 and tileY == 1:
+                print("checkpoint 1 achieved nigga")
+                checkpoint1 = True
+        if tileX == 27 and tileY == 9 and checkpoint1:
+                print("checkpoint 2 achieved nigga")
+                checkpoint2 = True
+        if tileX == 18 and tileY == 8 and checkpoint2:
+                print("checkpoint 3 achieved nigga")
+                checkpoint3 = True
+        if tileX == 1 and tileY == 4 and checkpoint3:
+                print("checkpoint 4 achieved nigga")
+                checkpoint4 = True
+        if tileX == 1 and tileY == 1 and checkpoint1 == True and checkpoint2 == True and checkpoint3 == True and checkpoint4 == True:
+                print("finished achieved nigga")
+                exit()
             
-    
-            
-    previousLocation = (player_car_rect.x, player_car_rect.y) # grabs location before collision
-    
-    player_car_rect.x += player_velocity.x
-    player_car_rect.y += player_velocity.y
+        if tile == COLLISION_TILE_ID: # checks if current tile location is allowed
+            player_car_rect.x = previousLocation[0] #revert to previous state 
+            player_car_rect.y = previousLocation[1] #revert to previous state 
 
-    tileX = math.floor(screen_to_world(player_car_rect.x, player_car_rect.y)[0])
-    tileY = math.floor(screen_to_world(player_car_rect.x, player_car_rect.y)[1])
-    xivo = screen_to_world(player_car_rect.x, player_car_rect.y)[0]
-    yivo = screen_to_world(player_car_rect.x, player_car_rect.y)[1]
-    print(f"x: {xivo:.2f} y: {yivo:.2f}, tileX: {tileX} tileY: {tileY}")
-    
-    tile_layer = tmx_map.get_layer_by_name("BORDERS")
-    tile = tile_layer.data[tileY][tileX] # gets current tile location
-    if tile == COLLISION_TILE_ID: # checks if current tile location is allowed
-        player_car_rect.x = previousLocation[0] #revert to previous state 
-        player_car_rect.y = previousLocation[1] #revert to previous state 
+        # Check if the player's car is colliding with the road
+        if player_velocity.length() > 0: # If the player's car is moving
+            player_angle = math.degrees(math.atan2(-player_velocity.y, player_velocity.x))  # Adjust the angle calculation
 
-    # Check if the player's car is colliding with the road
-    if player_velocity.length() > 0: # If the player's car is moving
-        player_angle = math.degrees(math.atan2(player_velocity.y, player_velocity.x)) # Get the angle of the player's car
+        # Rotate the player's car
+        rotated_car_image = pygame.transform.rotate(scaled_car_image, player_angle)  # Rotate the player's car
 
-    # Rotate the player's car
-    rotated_car_image = pygame.transform.rotate(scaled_car_image, player_angle)  # Rotate the player's car
+        # Clear the screen
+        screen.fill(WHITE)  # Fill the screen with white
 
-    # Clear the screen
-    screen.fill(WHITE)  # Fill the screen with white
+        # Draw the road layers
+        for layer in road_layers:  # Loop that draws the road layers
+            for x, y, image in layer.tiles():  # Loop that gets the tiles
+                scaled_image = pygame.transform.scale(image, (scaled_tilewidth, scaled_tileheight))  # Scale the image
+                screen.blit(scaled_image, (x * scaled_tilewidth, y * scaled_tileheight))  # Draw the image
 
-    # Draw the road layers
-    for layer in road_layers:  # Loop that draws the road layers
-        for x, y, image in layer.tiles():  # Loop that gets the tiles
-            scaled_image = pygame.transform.scale(image, (scaled_tilewidth, scaled_tileheight))  # Scale the image
-            screen.blit(scaled_image, (x * scaled_tilewidth, y * scaled_tileheight))  # Draw the image
+        # Draw the player's car
+        rotated_rect = rotated_car_image.get_rect(center=(player_car_rect.x, player_car_rect.y))  # Get the rectangle of the rotated car image
+        screen.blit(rotated_car_image, rotated_rect.topleft)  # Draw the rotated car image
 
-    # Draw the player's car
-    rotated_rect = rotated_car_image.get_rect(center=(player_car_rect.x, player_car_rect.y))  # Get the rectangle of the rotated car image
-    screen.blit(rotated_car_image, rotated_rect.topleft)  # Draw the rotated car image
+        # Update the display
+        pygame.display.flip()  # Update the display
 
-    # Update the display
-    pygame.display.flip()  # Update the display
-
-    # Set the FPS
-    clock.tick(FPS)  # Set the FPS
+        clock.tick(FPS)  # Set the FPS
+    elif game == 0:
+        screen.blit(backgrounds, (0,0))
+        screen.blit(img, imgRect)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            game = 1
+        pygame.display.flip()
+        clock.tick(FPS)
+        
 pygame.quit() # Quit pygame
 sys.exit() # Quit the program
