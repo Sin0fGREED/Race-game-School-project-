018815678b49a419319c4866ea42d595401e0c1d
GREED <pratikadhikari10@gmail.com>
2023-12-03 20:01:41 +0100

    Updated Variable names

--- Racing.py
+++ Racing.py
@@ -6,48 +6,55 @@ import math
 import pygame.mixer
 from vector2d import Vector2D
 import os
+
+
+# Initialize pygame
 pygame.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 1390, 768
-FPS = 60
-WHITE = (255, 255, 255)
-ROAD_COLOR = (100, 100, 100)
-MAP_SCALE = 3
-BLUE = (255, 255, 255)
-font = pygame.font.SysFont(None, 39)
-fontTimer = pygame.font.SysFont(None, 50)
-font_size = 50
-text_home = ("""Press ENTER for hard mode and BACKSPACE for easy mode. To win you must complete 3 laps of the track.""")
-img = font.render(text_home, True, BLUE)
-imgRect = img.get_rect()
-imgRect.center = (WIDTH // 2, HEIGHT // 2)
-imgRects = img.get_rect()
-imgRects.center = (WIDTH // 2, HEIGHT // 1.7)
-backgrounds = pygame.image.load("./mainMenu/car.jpg")
-lights = pygame.image.load("./mainMenu/lights.jpg")
-victory = pygame.image.load("./mainMenu/victory.jpeg")
-lose = pygame.image.load("./mainMenu/freddy.png")
-clock = pygame.time.Clock()
-counter = 5
-timer_event = pygame.USEREVENT + 1
-text = fontTimer.render(str(counter), True, (255, 0, 0))
-pygame.time.set_timer(timer_event, 1000)
-target_position = pygame.Vector2(1310, 55)
-drag = 0.04
-other_car_speed = 2
+WIDTH, HEIGHT = 1390, 768 # Screen dimensions
+FPS = 60 # Frames per second
+WHITE = (255, 255, 255) # White color
+ROAD_COLOR = (100, 100, 100) # Road color
+MAP_SCALE = 3 # Map scale
+BLUE = (255, 255, 255) # Blue color
+GREEN = (2, 247, 15) # Green color
+font = pygame.font.SysFont(None, 39) # Font 
+fontTimer = pygame.font.SysFont(None, 50)   # Font for timer
+font_size = 50   # Font size
+text_home = ("""Press ENTER for hard mode and BACKSPACE for easy mode. To win you must complete 3 laps of the track.""")    # Text for home screen
+Start_TXT = font.render(text_home, True, BLUE)  # Text for home screen
+Victory_TXT = font.render("You Won!", True, GREEN)  # Text for victory screen
+imgRect = Start_TXT.get_rect()  # Text for home screen
+imgRect.center = (WIDTH // 2, HEIGHT // 2)  # Text for home screen
+imgRects = Victory_TXT.get_rect()   # Text for victory screen
+imgRects.center = (WIDTH // 2, HEIGHT // 1.7)   # Text for victory screen
+backgrounds = pygame.image.load("./mainMenu/car.jpg")   # Background for home screen
+lights = pygame.image.load("./mainMenu/lights.jpg") # Background for timer screen
+victory = pygame.image.load("./mainMenu/victory.jpeg")  # Background for victory screen
+lose = pygame.image.load("./mainMenu/freddy.png")   # Background for lose screen
+clock = pygame.time.Clock() # Clock
+counter = 5 # Timer
+timer_event = pygame.USEREVENT + 1  # Timer event
+text = fontTimer.render(str(counter), True, (255, 0, 0))    # Timer text
+pygame.time.set_timer(timer_event, 1000)    # Timer event
+target_position = pygame.Vector2(1310, 55)  # Target position
+drag = 0.04 # Drag
+other_car_speed = 2 # AI car speed
+is_moving_to_target = True  # AI car movement
 
-is_moving_to_target = True
+# Create the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))   # Screen
+pygame.display.set_caption("Formula 1 Racing Course")   # Screen caption
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Formula 1 Racing Course")
+clock = pygame.time.Clock()     # Clock
 
-clock = pygame.time.Clock()
-
+# Game state variables
 frame_rate = 60
 frame_count = 0
 start_time = 0
 
+# Load the map
 tmx_map = pytmx.load_pygame('Road.tmx')
 road_layers = [layer for layer in tmx_map.visible_layers if isinstance(layer, pytmx.TiledTileLayer)]
 scaled_tilewidth = tmx_map.tilewidth * MAP_SCALE
@@ -55,32 +62,41 @@ scaled_tileheight = tmx_map.tileheight * MAP_SCALE
 scaled_width = tmx_map.width * scaled_tilewidth
 scaled_height = tmx_map.height * scaled_tileheight
 
+# Load the car image
 original_car_image = pygame.image.load("./Vehicles/CARS.png")
 scaled_car_image = pygame.transform.scale(original_car_image, (40, 20))
 
+# Load the AI car image
 ai_car_image = pygame.image.load("./Vehicles/aiCARS.png")
 ai_scaled_car_image = pygame.transform.scale(ai_car_image, (40, 20))
 
+# Create the player car rectangle
 player_car_rect = scaled_car_image.get_rect()
 player_car_rect.x = 150
 player_car_rect.y = 85
 
+# Create the AI car rectangle
 other_car_rect = ai_scaled_car_image.get_rect()
 other_car_rect.x = 150
 other_car_rect.y = 55
 
+# Create the player velocity vector
 player_velocity = pygame.Vector2(0, 0)
 player_angle = 0
 
 
-
 COLLISION_TILE_ID = 148
 
+# Function to convert screen coordinates to world coordinates
 def screen_to_world(screen_x, screen_y):
+    # Calculate the world x-coordinate based on the screen x-coordinate and the width of the screen
     world_x = (screen_x / WIDTH) * 29
+    # Calculate the world y-coordinate based on the screen y-coordinate and the height of the screen
     world_y = (screen_y / HEIGHT) * 16
+    # Return the world coordinates as a list [world_x, world_y]
     return [world_x, world_y]
 
+# Game state variables
 running = True
 checkpoint1 = False
 checkpoint2 = False
@@ -95,7 +111,7 @@ checkpoint10 = False
 checkpoint11 = False
 checkpoint12 = False
 
-
+# Game settings
 game = 0
 step1 = False
 step2 = False
@@ -103,16 +119,17 @@ step3 = False
 step5 = False
 play = False
 
-
+# Sound effects
 win_sound = pygame.mixer.Sound("win.wav")
 lose_sound = pygame.mixer.Sound("lose.wav")
 pygame.mixer.music.load("Music.wav")
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.15)
 
-
+# Sound effects volume
 win_sound.set_volume(2)
 
+# Game loop
 laps = 0
 lapsai = 0
 while running:
@@ -152,11 +169,11 @@ while running:
 
         tileX = math.floor(screen_to_world(player_car_rect.x, player_car_rect.y)[0])
         tileY = math.floor(screen_to_world(player_car_rect.x, player_car_rect.y)[1])
-
+        
         tile_layer = tmx_map.get_layer_by_name("BORDERS")
         tile = tile_layer.data[tileY][tileX]
         
-
+        # Checkpoints and laps
         if tileX == 10 and tileY == 1:
             print("checkpoint 1 achieved")
             checkpoint1 = True
@@ -229,13 +246,13 @@ while running:
             if direction.length() > 0:
                 direction.normalize_ip()
 
-            other_car_rect.x += direction.x * other_car_speed
+            other_car_rect.x += direction.x * other_car_speed 
             other_car_rect.y += direction.y * other_car_speed
-
+            
             angle = math.degrees(math.atan2(direction.y, direction.x))
             rotated_ai_car_image = pygame.transform.rotate(ai_scaled_car_image, -angle)
             rotated_ai_rect = rotated_ai_car_image.get_rect(center=(other_car_rect.centerx, other_car_rect.centery))
-
+            
             screen.blit(rotated_ai_car_image, rotated_ai_rect.topleft)
             print(f"x: {other_car_rect.x} y: {other_car_rect.y}")
             if other_car_speed == 4:
@@ -322,13 +339,14 @@ while running:
         text = fontTimer.render(f'Time: {frame_count // frame_rate:02}:{frame_count % frame_rate:02}', True, (0, 0, 0))
         screen.blit(text, [6, 6])
 
+        # Update the display
         pygame.display.flip()
         clock.tick(FPS)
 
+    
     elif game == 0:
         screen.blit(backgrounds, (0, 0))
-        screen.blit(img, imgRect)
-        
+        screen.blit(Start_TXT, imgRect)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RETURN]:
             other_car_speed = 4
@@ -344,25 +362,28 @@ while running:
         text_rect = text.get_rect(center=screen.get_rect().center)
         screen.blit(text, text_rect)
         pygame.display.flip()
+
     elif game == 3:
         screen.blit(victory, (0, 0))
         pygame.mixer.music.stop()
         win_sound.play()
-        win_sound.set_volume(15)
+        win_sound.set_volume(5)
         font_size = 120
         font = pygame.font.Font(None, font_size)
         text_rect = text.get_rect(center=screen.get_rect().center)
         text = fontTimer.render(f'Time: {frame_count // frame_rate:02}:{frame_count % frame_rate:02}', True, (2, 247, 15))
         screen.blit(text, text_rect,)
-        screen.blit(text, text_rect,)
+        screen.blit(Victory_TXT, imgRects)
         pygame.display.flip()
         turn = 1
+        
     elif game == 4:
         screen.blit(lose, (0, 0))
         lose_sound.play()
-        lose_sound.set_volume(15)
+        lose_sound.set_volume(10)
         pygame.display.flip()
         turn = 1
 
+# Quit the game	
 pygame.quit()
 sys.exit()
